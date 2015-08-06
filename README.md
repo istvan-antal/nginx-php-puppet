e1f07bb3867dd090ef2eb57c76e2b033f858f02f
Antal István Miklós <istvan.m.antal@gmail.com>
2015-08-06 18:45:45 +0100

    Initial prototype

---
+++ README.md
@@ -0,0 +1,9 @@
+# Setup
+
+Clone this project onto an Ubuntu server and run:
+
+```bash
+make
+```
+
+This will set up PHP-FPM with Nginx.
---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install puppet
+cd "$(dirname ${BASH_SOURCE[0]})"
+sudo puppet apply --verbose --debug --modulepath=./modules main.pp
