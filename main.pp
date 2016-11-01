c2ec2f737c160180b8bc197694c63b9c50040990
István Antal <istvan.m.antal@gmail.com>
2016-11-01 16:18:45 +0000

    Comment out config files

--- main.pp
+++ main.pp
@@ -10,17 +10,17 @@ node default {
         enable => true
     }
 
-    file { "/etc/php7/fpm/pool.d/www.conf":
-        require => Package["php7.0-fpm"],
-        source  => "puppet:///modules/main/www.conf",
-        notify  => Service["php7-fpm"]
-    }
+    #file { "/etc/php7/fpm/pool.d/www.conf":
+    #    require => Package["php7.0-fpm"],
+    #    source  => "puppet:///modules/main/www.conf",
+    #    notify  => Service["php7-fpm"]
+    #}
 
-    file { "/etc/php7/fpm/php.ini":
-        require => Package["php7.0-fpm"],
-        content  => template('main/php.ini.erb'),
-        notify  => Service["php7-fpm"]
-    }
+    #file { "/etc/php7/fpm/php.ini":
+    #    require => Package["php7.0-fpm"],
+    #    content  => template('main/php.ini.erb'),
+    #    notify  => Service["php7-fpm"]
+    #}
 
     package { "nginx": ensure => "installed" }
 
